43c0afed7f0405043324803afa0e383146bcd700
mfvazquez <mfvazquez@users.noreply.github.com>
2018-04-08 13:05:59 -0300

    cambie un poco los titulos

--- practica 4/Practica4.m
+++ practica 4/Practica4.m
@@ -1,18 +1,24 @@
-% Fecha de entrega TP4 9/4 antes de las 16hs
-% enviar el html o pdf junto al codigo comprimido a:
-% pelle.patricia@gmail.com 
+%% Práctica 4: Linear Predictive Coding(LPC)
+% Vázquez Matías - 
+% Padron:91523 - 
+% |mfvazquezfiuba@gmail.com|
 
+%% LPC y la envolvente del espectro
+% Se realizará un chequeo aplicando LPC a una sección específica
+% de ancho temporal fijo sobre una señal, para observar la envolvente
+% del espectro. 
+
+%% Constantes utilizadas
+% 
+% * |ventana|: el ancho de la ventana utiliaza, en mseg.
+% * |ancho|: es la cantidad de elementos del audio que componen 25 mseg de audio.
+% * |inicio|: subindice a partir del cual se aplica la ventana. 
+% * |M|: numero de coeficientes utilizados
 
 close all
 clear
 
-
-%% AUDIO
-
 [audio, Fs] = audioread('fantasia.wav');
-
-
-%% CONSTANTES
 
 ventana = 25e-3;
 ancho =  ventana  * Fs;
@@ -63,7 +69,6 @@ error = S_estimado-S;
 figure
 plot(t,error);
 legend('error')
-
 
 [error, zf] = filter([-1;B], 1, S);
 figure

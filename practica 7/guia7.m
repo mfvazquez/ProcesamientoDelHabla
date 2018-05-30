c56b79c9e7b0a60d6d55b076d9333cee30a3b4f0
mfvazquez <mfvazquez@users.noreply.github.com>
2018-05-30 18:56:29 -0300

    practica 7

--- practica 7/guia7.m
+++ practica 7/guia7.m
@@ -6,7 +6,7 @@ addpath('lib');
 load(fullfile('data','data.mat'));
 
 %% EJERCICIO 1
-
+% 
 % [x,stateSeq] = genhmm(hmm4.means,hmm4.vars,hmm4.trans);
 % figure(1)
 % plotseq(x,stateSeq)
@@ -26,8 +26,8 @@ trans = [0   1   0   0   0  ;...
 trans(trans<1e-100) = 1e-100;
 
 x = [];
-while length(x) < 5
-    [x,stateSeq] = genhmm(means,vars,trans);
+while length(x) < 100
+    [x,stateSeq] = genhmm(hmm4);
 end
 train_set = x';
 
@@ -39,9 +39,9 @@ vars(2:end-1) = {varianza_inicial};
 
 
 
-TOLERANCIA = 1e-6;
+TOLERANCIA = 1e-16;
 likelihood = [];
-for M = 1:20
+for M = 1:5
 
     %% OBTENGO GAMMA
 
@@ -71,13 +71,42 @@ for M = 1:20
          if rcond(var_actual) < TOLERANCIA
              break
          end
+         vars{k} = var_actual;
          
     end
 
     %% MATRIZ DE TRANSICION
-
+    xi_normal = exp(xi);
+    for j = 1:size(xi,1)
+        for k = 1:size(xi,2)           
+            numerador = sum(xi(j,k,2:end));
+            denominador = sum(sum(xi(j,:,2:end)));
+            trans(j,k) = numerador/denominador;            
+        end
+    end
     
     %% DIBUJAR COMO VA CATEGORIZANDO EN CADA ITERACION
-   
+
+    figure
+    hold on
+%     plotseq2(x,stateSeq)
+    colores = 'rgb';
+    
+    for t = 1:size(x,1)
+%         clase = find(Gamma(t,:) == max(Gamma(t,:)));
+%         plot(x(t,1),x(t,2),'o','color',colores(clase(1)));
+        aux = Gamma(t,:)*1000;
+        aux = floor(aux)/1000;
+            plot(x(t,1),x(t,2), 'o', 'color', aux)
+    end
+    
+    
+    for k = 2:length(means)-1
+        elipse = obtener_elipse(means{k}, vars{k});
+        plot(elipse(1,:),elipse(2,:), colores(k-1))
+        plot(means{k}(1), means{k}(2), ['+' colores(k-1)]);
+    end
+    
+    
 end % ESTO DEBERIA TERMINAR CUANDO P(X) ya no cambia, que seria el likelihood
 
